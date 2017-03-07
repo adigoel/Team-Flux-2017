bfff91e236be5ae0c0822576ec6dc42a6ae2d3b1
Aditya Goel <adigoel@users.noreply.github.com>
2017-03-07 11:46:07 +0000

    Add files via upload

---
+++ IMU_and_pressure_reader.ino
@@ -0,0 +1,145 @@
+/*
+The sensor outputs provided by the library are the raw 16-bit values
+obtained by concatenating the 8-bit high and low accelerometer and
+magnetometer data registers. They can be converted to units of g and
+gauss using the conversion factors specified in the datasheet for your
+particular device and full scale setting (gain).
+
+Example: An LSM303D gives a magnetometer X axis reading of 1982 with
+its default full scale setting of +/- 4 gauss. The M_GN specification
+in the LSM303D datasheet (page 10) states a conversion factor of 0.160
+mgauss/LSB (least significant bit) at this FS setting, so the raw
+reading of -1982 corresponds to 1982 * 0.160 = 317.1 mgauss =
+0.3171 gauss.
+
+Essentially this gives the acceleration and heading of our satellite.
+
+In the LSM303DLHC, LSM303DLM, and LSM303DLH, the acceleration data
+registers actually contain a left-aligned 12-bit number, so the lowest
+4 bits are always 0, and the values should be shifted right by 4 bits
+(divided by 16) to be consistent with the conversion factors specified
+in the datasheets.
+*/
+
+#include <Wire.h>
+#include <LSM303.h>
+#include <SFE_BMP180.h>
+
+SFE_BMP180 pressure;
+LSM303 compass;
+LSM303::vector<int16_t> running_min = {32767, 32767, 32767}, running_max = {-32768, -32768, -32768};
+
+char heading_rep[80];
+char calibrate_rep[80];
+#define ALTITUDE 1655.0
+
+void setup()
+{
+  Serial.begin(9600);
+  //baud rate redefined; note not all sensors must have same baud rates
+  Wire.begin();
+  compass.init();
+  compass.enableDefault();
+  compass.m_min = (LSM303::vector<int16_t>){-32767, -32767, -32767};
+  compass.m_max = (LSM303::vector<int16_t>){+32767, +32767, +32767};
+  if (pressure.begin()){
+    Serial.println("BMP180 init success");
+  }
+  else
+  {
+    Serial.println("BMP180 init fail\n\n");
+    while(1); // Pause forever. Note that in our operational code
+              // here we route back to another library not
+              // not refrenced here as part of our control
+              // guidance logic
+  }
+}
+
+
+
+void loop()
+{
+  compass.read();
+  //function to read  heading data
+  float heading = compass.heading();
+  char status;
+  double T,P,p0,a;
+  //defining some variables
+
+  //getting data
+  running_min.x = min(running_min.x, compass.m.x);
+  running_min.y = min(running_min.y, compass.m.y);
+  running_min.z = min(running_min.z, compass.m.z);
+
+  running_max.x = max(running_max.x, compass.m.x);
+  running_max.y = max(running_max.y, compass.m.y);
+  running_max.z = max(running_max.z, compass.m.z);
+  
+  snprintf(heading_rep, sizeof(heading_rep), "A: %6d %6d %6d    M: %6d %6d %6d",
+    compass.a.x, compass.a.y, compass.a.z,
+    compass.m.x, compass.m.y, compass.m.z);
+  snprintf(calibrate_rep, sizeof(calibrate_rep), "min: {%+6d, %+6d, %+6d}    max: {%+6d, %+6d, %+6d}",
+    running_min.x, running_min.y, running_min.z,
+    running_max.x, running_max.y, running_max.z);
+
+  Serial.println(heading);
+  Serial.println(heading_rep);
+  Serial.println(calibrate_rep);
+  
+  //if ok to recheck
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // waiting for the measurement to complete...
+    delay(status);
+    
+  
+  delay(100);
+  status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Print out the measurement:
+      Serial.print("temperature: ");
+      Serial.print(T,2);
+      Serial.print(" deg C, ");
+      Serial.print((9.0/5.0)*T+32.0,2);
+      Serial.println(" deg F");
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          // retrieved data
+          Serial.print("absolute pressure: ");
+          Serial.print(P,2);
+          Serial.print(" mb, ");
+          Serial.print(P*0.0295333727,2);
+          Serial.println(" inHg");
+          p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+          Serial.print("relative (sea-level) pressure: ");
+          Serial.print(p0,2);
+          Serial.print(" mb, ");
+          Serial.print(p0*0.0295333727,2);
+          Serial.println(" inHg");
+        a = pressure.altitude(P,p0);
+          Serial.print("computed altitude: ");
+          Serial.print(a,0);
+          Serial.print(" meters, ");
+          Serial.print(a*3.28084,0);
+          Serial.println(" feet");
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+
+  delay(5000);  // Pause for 5 seconds.
+}
+
+
